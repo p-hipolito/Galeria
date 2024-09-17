8480ae432ddd97e5c7c88381366ceadbe62e712d
Paulo <129566106+p-hipolito@users.noreply.github.com>
2024-09-17 18:07:19 -0300

    added button to toggle password visibility

--- Login/Scripts/stylescript.js
+++ Login/Scripts/stylescript.js
@@ -5,8 +5,11 @@ const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
 const btnPopup = document.querySelector(".btnLogin-popup");
 const iconClose = document.querySelector(".icon-close");
-const registerBtn = document.getElementById('register-btn');
-
+const registerBtn = document.querySelector('#register-btn');
+const lock = document.querySelector('#lock');
+const reLock = document.querySelector('#reLock');
+const passwordInput = document.querySelector('#passwordInput');
+const registerPassword = document.querySelector('#registerPassword');
 
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -23,6 +26,29 @@ btnPopup.addEventListener('click', () => {
 iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 })
+
+
+// Changing login password visibility to user
+function passwordDisplay( ) {
+    if(passwordInput.type ==='password') {
+        passwordInput.type = 'text';
+        lock.querySelector('ion-icon').setAttribute('name', 'lock-open');
+    } else {
+        passwordInput.type ='password';
+        lock.querySelector('ion-icon').setAttribute('name', 'lock-closed');
+    };
+};
+
+// Changing register password visibility to user
+function registerPasswordDisplay( ) {
+    if(registerPassword.type ==='password') {
+        registerPassword.type = 'text';
+        reLock.querySelector('ion-icon').setAttribute('name', 'lock-open');
+    } else {
+        registerPassword.type ='password';
+        reLock.querySelector('ion-icon').setAttribute('name', 'lock-closed');
+    };
+};
 
 //Apply style to email label even if invalid
 
